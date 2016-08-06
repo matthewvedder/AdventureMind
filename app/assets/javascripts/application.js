b3ced5ca01a935b36618551e89db986563351bec
veddster <matthewvedder@gmail.com>
2016-08-06 18:30:52 -0400

    move leaflet js to map.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,14 +16,3 @@
 //= require_tree .
 
 
-$( document ).ready(function() {
-  var mymap = L.map('mapid').setView([42.0000, -97.0000], 4.2);
-
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-      maxZoom: 18,
-      id: 'veddster.0pl6d53m',
-      accessToken: 'pk.eyJ1IjoidmVkZHN0ZXIiLCJhIjoiY2lyNzdlanUzMDBza2djbTM1Z2hlYTdnNSJ9.FpydM0KRrrunXzaHJYCmrA'
-  }).addTo(mymap);
-
-});
