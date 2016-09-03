f775e0397a7d81043f0f5f3065519b98801f78d2
veddster <matthewvedder@gmail.com>
2016-09-03 17:48:11 -0400

    change set view back to US in map.js

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-  var map = L.map('mapid', { zoomControl:false }).locate({setView: true, maxZoom: 16});
+  var map = L.map('mapid', { zoomControl:false }).setView([42.0000, -97.0000], 4.2);
 
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
